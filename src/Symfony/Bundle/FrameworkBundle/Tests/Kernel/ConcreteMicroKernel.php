39068df9786d05100b8baf608aaad00c791b633c
Ashley Dawson <ashley.dawson@deskpro.com>
2024-02-21 21:51:31 +0000

    Patched for CVE-2019-10912

--- src/Symfony/Bundle/FrameworkBundle/Tests/Kernel/ConcreteMicroKernel.php
+++ src/Symfony/Bundle/FrameworkBundle/Tests/Kernel/ConcreteMicroKernel.php
@@ -48,6 +48,16 @@ class ConcreteMicroKernel extends Kernel
         return $this->cacheDir;
     }
 
+    public function __sleep()
+    {
+        throw new \BadMethodCallException('Cannot serialize '.__CLASS__);
+    }
+
+    public function __wakeup()
+    {
+        throw new \BadMethodCallException('Cannot unserialize '.__CLASS__);
+    }
+
     public function __destruct()
     {
         $fs = new Filesystem();
